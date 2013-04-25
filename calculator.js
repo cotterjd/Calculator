a6dd17f1ddbfdc0ec9f6ff33ca42d46009fa74e8
cotterjd <jrdn.cttr@gmail.com>
2013-04-25 15:30:06 -0500

    hitting enter will repeat previous operation and can now do operations to products of previous operations

--- calculator.js
+++ calculator.js
@@ -11,10 +11,14 @@ var numberOne = "";
 var numberTwo = "";
 var numberTracker = 0;
 var operation = "";
-var equalsString = " = <br />-----------------------<br />";
+var equalsString = " = <br />--------------------------<br />";
 
 function clicknumbers() {
     $('.number').click(function () {
+        if (lastInput == "=") {
+            resetValues();
+            $('#calculations').append("<br /></br />");
+        }
         if (numberTracker == 0) {
             numberOne += $(this).val();
             $('#calculations').append($(this).val());
@@ -26,6 +30,9 @@ function clicknumbers() {
             lastInput = "number";
         }
         else {//after numbtracker=1, numberOne is no longer defined in this function
+            if (lastInput != "number") {
+                numberTwo = "";
+            }
             numberTwo += $(this).val();
             if (operation == "/" && numberTwo == 0) {
                 alert("Division by 0 is not defined in mathematics");
@@ -41,6 +48,7 @@ function clicknumbers() {
                 lastInput = "number";
             }
         }
+        test();
     });
 }//end clicknumbers
 function clickOperation() {
@@ -48,7 +56,7 @@ function clickOperation() {
         if (lastInput == "number") {
             numberTracker++;
         }   //prevent duplicat operations//allows negative numbers, but not duplicat negatives//allows subtraction of negative numbers
-        if (lastInput == "number" || ($(this).val() == "-" && lastInput != "-") || ($(this).val() == "-" && beforeLastInput == "number")) {
+        if (lastInput == "number" || ($(this).val() == "-" && lastInput != "-") || ($(this).val() == "-" && beforeLastInput == "number") || (lastInput == "=" && numberOne != "")) {
             if (numberTracker != 0 && numberTracker != 1) {
                 numberOne = parseFloat(numberOne);
                 if (numberTwo != "") {
@@ -67,7 +75,7 @@ function clickOperation() {
                     }
                     numberTwo = "";
                 }
-            }// end if
+            } // end if
             if (!($(this).val() == "-" && lastInput != "number")) {//if this input is not a minus sign
                 operation = $(this).val();
                 beforeLastInput = lastInput;
@@ -80,21 +88,27 @@ function clickOperation() {
                 $('#calculations').append(" " + $(this).val());
             }
         }
+        test();
     });
 }
 
 function enter() {
     $('#enter').click(function () {
         if ((numberOne != "" && numberTwo != "" && operation != "") || (numberOne != "" && numberTwo == "" && (operation == "" || operation == "-"))) {
+            if (lastInput == "=") {
+                $('#calculations').append(" " + operation + "<br />" + numberTwo);
+            }
             numberOne = parseFloat(numberOne);
             if (numberTwo != "") {
                 numberTwo = parseFloat(numberTwo);
             }
             if (operation == "x") {
                 $('#calculations').append(equalsString + numberOne * numberTwo);
+                numberOne = numberOne * numberTwo;
             }
             else if (operation == "+") {
                 $('#calculations').append(equalsString + (numberOne + numberTwo));
+                numberOne = numberOne + numberTwo;
             }
             else if (operation == "-") {
                 if (numberTwo == "") {
@@ -102,17 +116,19 @@ function enter() {
                 }
                 else {
                     $('#calculations').append(equalsString + (numberOne - numberTwo));
+                    numberOne = numberOne - numberTwo;
                 }
             }
             else if (operation == "/") {
                 $('#calculations').append(equalsString + numberOne / numberTwo);
+                numberOne = numberOne / numberTwo;
             }
             else {
                 $('#calculations').append(equalsString + numberOne);
             }
-            $('#calculations').append("<br /></br />");
-            resetValues();
+            lastInput = "=";
         }
+        test();
     });
 }
 
@@ -120,8 +136,8 @@ function clear() {
     $('#clear').click(function () {
         $('#calculations').empty();
         resetValues();
+        test();
     });
-    resetValue();
 }
 
 function resetValues() {
@@ -132,3 +148,13 @@ function resetValues() {
     lastInput = "";
     beforeLastInput = "";
 }
+
+function test() {
+    console.log("operation: " + operation + "\n" +
+            "lastInput: " + lastInput + "\n" +
+            "beforeLastInput: " + beforeLastInput + "\n" +
+            "numberOne: " + numberOne + "\n" +
+            "numberTwo: " + numberTwo + "\n" +
+            "numberTracker: " + numberTracker + "\n");
+}
+
